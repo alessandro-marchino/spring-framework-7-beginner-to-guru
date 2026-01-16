f915e003a16f14fb6f65cd80ffb47e0e71599f76
amarchino <marchino.alessandro@gmail.com>
2026-01-16 23:17:56 +0100

    Assignment 3

---
+++ spring7di/src/main/java/guru/springframework/spring7di/Spring7diApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7diApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7diApplication.class, args);
+	}
+
+}
---
+++ spring7di/src/test/java/guru/springframework/spring7di/Spring7diApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7diApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
