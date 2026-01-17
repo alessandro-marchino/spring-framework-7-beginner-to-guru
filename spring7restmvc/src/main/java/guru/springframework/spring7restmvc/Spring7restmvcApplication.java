06c61e011b3c433b96115964a7a8d2e4da903438
amarchino <marchino.alessandro@gmail.com>
2026-01-17 20:54:49 +0100

    Lesson 52

---
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/Spring7restmvcApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7restmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7restmvcApplication.class, args);
+	}
+
+}
