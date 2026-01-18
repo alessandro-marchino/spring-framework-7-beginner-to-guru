025c582bb086c2ada2b00accaea121c41c37cc10
amarchino <marchino.alessandro@gmail.com>
2026-01-18 15:56:07 +0100

    Lesson 74

--- spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
+++ spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerTest.java
@@ -1,13 +1,11 @@
 package guru.springframework.spring7restmvc.controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-import java.util.UUID;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +26,13 @@ public class BeerControllerTest {
 	@Test
 	void testGetBeerById() throws Exception {
 		Beer testBeer = new BeerServiceImpl().listBeers().get(0);
-		given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+		given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
-		mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID())
+		mockMvc.perform(get("/api/v1/beer/{beerId}", testBeer.getId())
 				.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andExpect(content()
-				.contentType(MediaType.APPLICATION_JSON));
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+			.andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
 	}
 }
