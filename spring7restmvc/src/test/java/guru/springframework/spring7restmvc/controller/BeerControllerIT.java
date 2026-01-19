1c4a671fd6b353373e927f8d1246d52b1b45aae8
amarchino <marchino.alessandro@gmail.com>
2026-01-19 23:30:34 +0100

    Lesson 125

--- spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
+++ spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
@@ -23,7 +23,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
