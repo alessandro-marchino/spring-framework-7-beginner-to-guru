65a2ce27dab615434cb4d60bd1868ae0992d3487
amarchino <marchino.alessandro@gmail.com>
2026-01-19 20:46:52 +0100

    Lesson 117

--- spring7restmvc/src/main/java/guru/springframework/spring7restmvc/entities/Beer.java
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/entities/Beer.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import guru.springframework.spring7restmvc.model.BeerStyle;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
@@ -17,6 +18,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,11 +42,14 @@ public class Beer {
 
 	@NotNull
 	@NotBlank
+	@Size(max = 50)
+	@Column(length = 50)
 	private String beerName;
 	@NotNull
 	private BeerStyle beerStyle;
 	@NotNull
 	@NotBlank
+	@Size(max = 255)
 	private String upc;
 	@PositiveOrZero
 	private Integer quantityOnHand;
--- spring7restmvc/src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
+++ spring7restmvc/src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7restmvc.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import guru.springframework.spring7restmvc.entities.Beer;
 import guru.springframework.spring7restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -25,5 +27,15 @@ class BeerRepositoryTest {
 			.build());
 		assertThat(savedBeer).isNotNull();
 		assertThat(savedBeer.getId()).isNotNull();
+	}
+
+	@Test
+	void testSaveBeerTooLong() {
+		assertThrows(ConstraintViolationException.class, () -> repository.saveAndFlush(Beer.builder()
+			.beerName("My beer 12345678901234567890123456789012345678901234567890")
+			.beerStyle(BeerStyle.GOSE)
+			.upc("123456789")
+			.price(new BigDecimal("12.99"))
+			.build()));
 	}
 }
