97589208c1740908ca0f13a99688a2f1bbeb1f70
amarchino <marchino.alessandro@gmail.com>
2026-01-21 23:10:29 +0100

    Lesson 136

--- spring7restmvc/src/main/java/guru/springframework/spring7restmvc/entities/Customer.java
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/entities/Customer.java
@@ -35,6 +35,7 @@ public class Customer {
 	@Version
 	private Integer version;
 	private String customerName;
+	private String email;
 	private LocalDateTime createdDate;
 	private LocalDateTime updatedDate;
 }
