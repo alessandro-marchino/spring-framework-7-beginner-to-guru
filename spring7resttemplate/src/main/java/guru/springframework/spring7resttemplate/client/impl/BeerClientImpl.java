d47f50bc9f9e9f16f2d483b564785985ea13776b
amarchino <marchino.alessandro@gmail.com>
2026-01-25 22:45:20 +0100

    Lesson 203

--- spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImpl.java
+++ spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7resttemplate.client.impl;
 
 import java.net.URI;
+import java.util.Map;
 
 import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -18,14 +19,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BeerClientImpl implements BeerClient {
 
+	private static final String BASE_URL = "http://localhost:8080";
+	private static final String GET_BEER_PATH = "/api/v1/beer";
+
 	private final RestTemplateBuilder restTemplateBuilder;
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	public Page<BeerDTO> listBeers() {
-		RestTemplate restTemplate = restTemplateBuilder.build();
+		RestTemplate restTemplate = restTemplateBuilder.rootUri(BASE_URL).build();
 
-		ResponseEntity<String> stringResponse = restTemplate.getForEntity(URI.create("http://localhost:8080/api/v1/beer"), String.class);
+		ResponseEntity<String> stringResponse = restTemplate.getForEntity(GET_BEER_PATH, String.class);
 		log.warn("Body: {}", stringResponse.getBody());
+
+		ResponseEntity<Map> mapResponse = restTemplate.getForEntity(GET_BEER_PATH, Map.class);
+		log.warn("Body: {}", mapResponse.getBody());
 		// TODO Auto-generated method stub
 		return null;
 	}
