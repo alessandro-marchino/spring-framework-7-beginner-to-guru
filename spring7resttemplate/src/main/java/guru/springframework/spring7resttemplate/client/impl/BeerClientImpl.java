cb2011ecb4a1f892b2c675d542306a1eaf951638
amarchino <marchino.alessandro@gmail.com>
2026-01-25 22:38:52 +0100

    Lesson 202

--- spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImpl.java
+++ spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImpl.java
@@ -1,14 +1,31 @@
 package guru.springframework.spring7resttemplate.client.impl;
 
+import java.net.URI;
+
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import guru.springframework.spring7resttemplate.client.BeerClient;
 import guru.springframework.spring7resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class BeerClientImpl implements BeerClient {
+
+	private final RestTemplateBuilder restTemplateBuilder;
 
 	@Override
 	public Page<BeerDTO> listBeers() {
+		RestTemplate restTemplate = restTemplateBuilder.build();
+
+		ResponseEntity<String> stringResponse = restTemplate.getForEntity(URI.create("http://localhost:8080/api/v1/beer"), String.class);
+		log.warn("Body: {}", stringResponse.getBody());
 		// TODO Auto-generated method stub
 		return null;
 	}
--- spring7resttemplate/src/test/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImplTest.java
+++ spring7resttemplate/src/test/java/guru/springframework/spring7resttemplate/client/impl/BeerClientImplTest.java
@@ -1,11 +1,16 @@
 package guru.springframework.spring7resttemplate.client.impl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+	@Autowired BeerClientImpl beerClientImpl;
 
     @Test
     void testListBeers() {
-		// TODO
+		beerClientImpl.listBeers();
     }
 }
