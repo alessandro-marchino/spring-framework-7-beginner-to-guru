6a6be994f074888ae287a5fd5ab3e0bd9473a44d
amarchino <marchino.alessandro@gmail.com>
2026-01-31 23:49:23 +0100

    Lesson 262

---
+++ spring7reactiveexamples/src/main/java/guru/springframework/spring7reactiveexamples/Spring7reactiveexamplesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7reactiveexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7reactiveexamplesApplication.class, args);
+	}
+
+}
---
+++ spring7reactiveexamples/src/test/java/guru/springframework/spring7reactiveexamples/Spring7reactiveexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7reactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7reactiveexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
