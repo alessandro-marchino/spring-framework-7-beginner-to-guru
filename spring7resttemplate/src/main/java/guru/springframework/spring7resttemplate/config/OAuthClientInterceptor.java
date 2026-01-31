f1c13b3e031d4796c68bcfab104e92c84057ee0a
amarchino <marchino.alessandro@gmail.com>
2026-01-31 19:32:03 +0100

    Lesson 256

---
+++ spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/config/OAuthClientInterceptor.java
@@ -0,0 +1,83 @@
+package guru.springframework.spring7resttemplate.config;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+public class OAuthClientInterceptor implements ClientHttpRequestInterceptor {
+	private final OAuth2AuthorizedClientManager manager;
+	private final ClientRegistration clientRegistration;
+
+	public OAuthClientInterceptor(OAuth2AuthorizedClientManager manager, ClientRegistrationRepository clientRegistrationRepository) {
+		this.manager = manager;
+		this.clientRegistration = clientRegistrationRepository.findByRegistrationId("springauth");
+	}
+
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+		OAuth2AuthorizeRequest oAuth2AuthorizeRequest = OAuth2AuthorizeRequest
+			.withClientRegistrationId(clientRegistration.getRegistrationId())
+			.principal(createPrincipal())
+			.build();
+		OAuth2AuthorizedClient client = manager.authorize(oAuth2AuthorizeRequest);
+		if (client == null) {
+			throw new IllegalStateException("Missing credentials");
+		}
+		request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken().getTokenValue());
+		return execution.execute(request, body);
+	}
+
+	private Authentication createPrincipal() {
+		return new CustomAuthentication(clientRegistration);
+    }
+
+	@RequiredArgsConstructor
+	private static class CustomAuthentication implements Authentication {
+		private final ClientRegistration clientRegistration;
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			return Collections.emptySet();
+		}
+		@Override
+		public Object getCredentials() {
+			return null;
+		}
+		@Override
+		public Object getDetails() {
+			return null;
+		}
+		@Override
+		public Object getPrincipal() {
+			return this;
+		}
+		@Override
+		public boolean isAuthenticated() {
+			return false;
+		}
+		@Override
+		public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+			// Empty
+		}
+		@Override
+		public String getName() {
+			return clientRegistration.getClientId();
+		}
+	}
+}
--- spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/config/RestTemplateBuilderConfig.java
+++ spring7resttemplate/src/main/java/guru/springframework/spring7resttemplate/config/RestTemplateBuilderConfig.java
@@ -7,6 +7,12 @@ import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.boot.restclient.autoconfigure.RestTemplateBuilderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
@@ -16,13 +22,24 @@ public class RestTemplateBuilderConfig {
 	String rootUrl;
 
 	@Bean
-	RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
+    AuthorizedClientServiceOAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientService oAuth2AuthorizedClientService ){
+		OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+			.clientCredentials()
+			.build();
+		AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+		return authorizedClientManager;
+	}
+
+	@Bean
+	RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer, OAuthClientInterceptor interceptor, OAuth2AuthorizedClientManager authorizedClientManager) {
 		assert rootUrl != null;
 		DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
 
 		return configurer.configure(new RestTemplateBuilder())
 			.uriTemplateHandler(defaultUriBuilderFactory)
 			.connectTimeout(Duration.ofSeconds(10))
-			.readTimeout(Duration.ofSeconds(20));
+			.readTimeout(Duration.ofSeconds(20))
+			.additionalInterceptors(interceptor);
 	}
 }
