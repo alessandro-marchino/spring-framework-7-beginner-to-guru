9549a559b41210057a275a128b39af49219d2121
amarchino <marchino.alessandro@gmail.com>
2026-02-01 23:19:54 +0100

    Lesson 265

--- spring7reactiveexamples/src/test/java/guru/springframework/spring7reactiveexamples/repository/impl/PersonRepositoryImplTest.java
+++ spring7reactiveexamples/src/test/java/guru/springframework/spring7reactiveexamples/repository/impl/PersonRepositoryImplTest.java
@@ -2,10 +2,13 @@ package guru.springframework.spring7reactiveexamples.repository.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import guru.springframework.spring7reactiveexamples.domain.Person;
 import guru.springframework.spring7reactiveexamples.repository.PersonRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 class PersonRepositoryImplTest {
@@ -19,18 +22,43 @@ class PersonRepositoryImplTest {
 		Person person = personMono.block();
 		assertThat(person.getFirstName()).isEqualTo("Michael");
 	}
-
 	@Test
 	void testGetByIdSubscriber() {
 		Mono<Person> personMono = personRepository.getById(1);
 		personMono.subscribe(person -> assertThat(person.getFirstName()).isEqualTo("Michael"));
 	}
-
 	@Test
 	void testMapOperation() {
 		Mono<Person> personMono = personRepository.getById(1);
 		personMono
 			.map(Person::getFirstName)
 			.subscribe(firstName -> assertThat(firstName).isEqualTo("Michael"));
+	}
+
+	@Test
+	void testFluxBlock() {
+		// Not preferred
+		Flux<Person> personFlux = personRepository.findAll();
+		Person person = personFlux.blockFirst();
+		assertThat(person.getFirstName()).isEqualTo("Michael");
+	}
+	@Test
+	void testFluxSubscriber() {
+		Flux<Person> personFlux = personRepository.findAll();
+		personFlux.subscribe(person -> assertThat(person).isNotNull());
+	}
+	@Test
+	void testFluxMap() {
+		Flux<Person> personFlux = personRepository.findAll();
+		personFlux
+			.map(Person::getFirstName)
+			.subscribe(firstName -> assertThat(firstName).isNotBlank());
+	}
+	@Test
+	void testFluxToList() {
+		Flux<Person> personFlux = personRepository.findAll();
+		Mono<List<Person>> listMono = personFlux.collectList();
+		listMono
+			.subscribe(list -> assertThat(list).hasSize(4));
 	}
 }
