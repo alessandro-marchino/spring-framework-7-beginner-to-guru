e96c410adedbfd70d67b28bbb3117d77f5408555
amarchino <marchino.alessandro@gmail.com>
2026-02-04 22:56:12 +0100

    Lesson 279

---
+++ spring7reactive/src/main/java/guru/springframework/spring7reactive/controllers/BeerController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring7reactive.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import guru.springframework.spring7reactive.model.BeerDTO;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(BeerController.BEER_PATH)
+public class BeerController {
+
+	public static final String BEER_PATH = "/api/v2/beer";
+
+	@GetMapping
+	public Flux<BeerDTO> listBeers() {
+		return Flux.just(
+			BeerDTO.builder().id(1).build(),
+			BeerDTO.builder().id(2).build()
+		);
+	}
+}
