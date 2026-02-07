528f45210b663a4d2991c4b99f70ba6d3146552a
amarchino <marchino.alessandro@gmail.com>
2026-02-07 22:05:05 +0100

    Lesson 290

--- spring7reactive/src/test/java/guru/springframework/spring7reactive/controllers/BeerControllerTest.java
+++ spring7reactive/src/test/java/guru/springframework/spring7reactive/controllers/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring7reactive.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +9,8 @@ import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTest
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import guru.springframework.spring7reactive.model.BeerDTO;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -25,7 +29,13 @@ class BeerControllerTest {
 
     @Test
     void testGetBeerById() {
-
+		webTestClient.get().uri(BeerController.BEER_PATH + BeerController.BEER_PATH_ID, 1)
+			.exchange()
+			.expectStatus().isOk()
+			.expectHeader().valueEquals(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.expectBody(BeerDTO.class).value(beerDto -> {
+				assertThat(beerDto.getId()).isEqualTo(1L);
+			});
     }
 
     @Test
