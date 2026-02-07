ba61854e47eb61aef1c3e50fee9ca60496f1c8c2
amarchino <marchino.alessandro@gmail.com>
2026-02-07 21:35:44 +0100

    Lesson 287

--- spring7reactive/src/main/java/guru/springframework/spring7reactive/controllers/BeerController.java
+++ spring7reactive/src/main/java/guru/springframework/spring7reactive/controllers/BeerController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring7reactive.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -39,13 +40,13 @@ public class BeerController {
 	}
 
 	@PostMapping
-	public Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
+	public Mono<ResponseEntity<Void>> createNewBeer(@RequestBody @Validated BeerDTO beerDTO) {
 		return beerService.saveNewBeer(beerDTO)
 			.map(savedDto -> ResponseEntity.created(UriComponentsBuilder.fromUriString("http://localhost:8080" + BEER_PATH + BEER_PATH_ID).build(savedDto.getId())).build());
 	}
 
 	@PutMapping(BEER_PATH_ID)
-	public Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable Integer beerId, @RequestBody BeerDTO beerDTO) {
+	public Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable Integer beerId, @RequestBody @Validated BeerDTO beerDTO) {
 		return beerService.updateBeer(beerId, beerDTO)
 			.map(_ -> ResponseEntity.ok().build());
 	}
--- spring7reactive/src/main/java/guru/springframework/spring7reactive/model/BeerDTO.java
+++ spring7reactive/src/main/java/guru/springframework/spring7reactive/model/BeerDTO.java
@@ -3,6 +3,10 @@ package guru.springframework.spring7reactive.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +19,16 @@ import lombok.NoArgsConstructor;
 public class BeerDTO {
 
 	private Integer id;
+	@NotBlank
+	@Size(min = 3, max = 255)
 	private String beerName;
+	@Size(min = 1, max = 255)
 	private String beerStyle;
+	@Size(max = 25)
 	private String upc;
+	@PositiveOrZero
 	private Integer quantityOnHand;
+	@Positive
 	private BigDecimal price;
 	private LocalDateTime createdDate;
 	private LocalDateTime lastModifiedDate;
