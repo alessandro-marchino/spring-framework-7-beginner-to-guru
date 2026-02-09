0e7a0c91c895ee3f58d0c30ee35fc56775938a6d
amarchino <marchino.alessandro@gmail.com>
2026-02-09 23:25:07 +0100

    Lesson 304

---
+++ spring7reactivemongo/src/test/java/guru/springframework/spring7reactivemongo/Spring7reactivemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7reactivemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
