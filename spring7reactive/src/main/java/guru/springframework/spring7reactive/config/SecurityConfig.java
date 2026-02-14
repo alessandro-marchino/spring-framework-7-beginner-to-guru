2893f10d48e78be1df134e37b272f41223b02e60
amarchino <marchino.alessandro@gmail.com>
2026-02-14 22:29:47 +0100

    Lesson 340

---
+++ spring7reactive/src/main/java/guru/springframework/spring7reactive/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring7reactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+			.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+			.oauth2ResourceServer(oauth2ResourceServerSpec -> oauth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+			.build();
+	}
+}
