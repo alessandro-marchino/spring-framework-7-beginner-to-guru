c9971e00ee04d8b1c2755b3368e538838f6c4ced
amarchino <marchino.alessandro@gmail.com>
2026-02-14 12:16:03 +0100

    Lesson 329

---
+++ spring7webclient/src/test/java/guru/springframework/spring7webclient/Spring7webclientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7webclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
