fbff34ce2cbe7a91f285f9d20e933a5f6409c445
amarchino <marchino.alessandro@gmail.com>
2026-02-15 13:13:50 +0100

    Lesson 349

--- spring7webclient/src/main/java/guru/springframework/spring7webclient/config/WebClientConfig.java
+++ spring7webclient/src/main/java/guru/springframework/spring7webclient/config/WebClientConfig.java
@@ -3,21 +3,29 @@ package guru.springframework.spring7webclient.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
 	private final String rootUrl;
+	private final ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager;
 
-	public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl) {
+	public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl, ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager) {
 		this.rootUrl = rootUrl;
+		this.reactiveOAuth2AuthorizedClientManager = reactiveOAuth2AuthorizedClientManager;
 	}
 
 	@Bean
 	WebClient webClient(WebClient.Builder builder) {
+		ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(reactiveOAuth2AuthorizedClientManager);
+		oauth.setDefaultClientRegistrationId("springauth");
+
 		return builder
 			.baseUrl(rootUrl)
+			.filter(oauth)
 			.build();
 	}
 }
