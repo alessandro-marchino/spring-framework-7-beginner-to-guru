d1adcc799cf60cd0669767b1461f91b3ae68aeec
amarchino <marchino.alessandro@gmail.com>
2026-02-16 21:13:55 +0100

    Lesson 351

---
+++ spring7gateway/src/main/java/guru/springframework/spring7gateway/Spring7gatewayApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7gatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7gatewayApplication.class, args);
+	}
+
+}
