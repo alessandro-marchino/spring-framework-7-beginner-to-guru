ca7b741785563569d9e0926bdfb8352542e6f707
amarchino <marchino.alessandro@gmail.com>
2026-02-20 16:40:15 +0100

    Lesson 380

--- spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerRestAssuredTest.java
+++ spring7restmvc/src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerRestAssuredTest.java
@@ -5,13 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 
+@ActiveProfiles("test-restassured")
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan("guru.springframework.spring7restmvc")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BeerControllerRestAssuredTest {
+
+	@Configuration
+	public static class TestConfig {
+		@Bean
+		DefaultSecurityFilterChain filterChain(HttpSecurity http) {
+			return http
+				.authorizeHttpRequests(spec -> spec.anyRequest().permitAll())
+				.build();
+		}
+	}
 
 	@LocalServerPort
 	Integer localPort;
@@ -26,7 +46,7 @@ public class BeerControllerRestAssuredTest {
 	void testListBeers() {
 		given().contentType(ContentType.JSON)
 			.when()
-				.get("/apiv1/beer")
+				.get("/api/v1/beer")
 			.then()
 				.assertThat().statusCode(200);
 	}
