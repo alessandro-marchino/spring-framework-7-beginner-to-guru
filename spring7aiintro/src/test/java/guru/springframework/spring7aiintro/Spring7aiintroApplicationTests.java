a064a5d4cd69fbdfc410709f9f4b011a782faccd
amarchino <marchino.alessandro@gmail.com>
2026-02-21 21:44:33 +0100

    Lesson 390

---
+++ spring7aiintro/src/test/java/guru/springframework/spring7aiintro/Spring7aiintroApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring7aiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7aiintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
