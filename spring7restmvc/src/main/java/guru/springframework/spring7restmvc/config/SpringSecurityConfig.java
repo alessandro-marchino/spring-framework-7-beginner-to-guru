ceb680c7c400b809835777ccf57b9c30e5f15fea
amarchino <marchino.alessandro@gmail.com>
2026-02-22 23:04:10 +0100

    Lesson 412

--- spring7restmvc/src/main/java/guru/springframework/spring7restmvc/config/SpringSecurityConfig.java
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/config/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring7restmvc.config;
 
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
@@ -12,6 +14,7 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 public class SpringSecurityConfig {
 
 	@Bean
+	@Order(2)
 	DefaultSecurityFilterChain filterChain(HttpSecurity http) {
 		return http
 			.authorizeHttpRequests(spec ->
@@ -20,6 +23,15 @@ public class SpringSecurityConfig {
 					.anyRequest().authenticated())
 			.csrf(csrfCustomizer -> csrfCustomizer.ignoringRequestMatchers("/api/**"))
 			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+			.build();
+	}
+
+	@Bean
+	@Order(1)
+	DefaultSecurityFilterChain actuatorFilterChain(HttpSecurity http) {
+		return http
+			.securityMatchers(spec -> spec.requestMatchers(EndpointRequest.toAnyEndpoint()))
+			.authorizeHttpRequests(spec -> spec.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll())
 			.build();
 	}
 }
