5a4c063b2fd8a62eaa7733106d40e0817284011f
amarchino <marchino.alessandro@gmail.com>
2026-02-23 23:25:07 +0100

    Lesson 425

--- spring7restmvc/src/main/java/guru/springframework/spring7restmvc/Spring7restmvcApplication.java
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/Spring7restmvcApplication.java
@@ -2,10 +2,12 @@ package guru.springframework.spring7restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableCaching
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class Spring7restmvcApplication {
 
