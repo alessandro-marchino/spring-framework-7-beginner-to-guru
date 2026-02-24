eee06741da35e62a44656a47b6b1ab3b40e2a7dd
amarchino <marchino.alessandro@gmail.com>
2026-02-24 23:07:36 +0100

    Lesson 432

---
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/events/BeerCreatedEvent.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring7restmvc.events;
+
+import org.springframework.security.core.Authentication;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class BeerCreatedEvent {
+	private Beer beer;
+	private Authentication authentication;
+}
