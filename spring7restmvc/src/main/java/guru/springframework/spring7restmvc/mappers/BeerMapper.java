32fa29714a3bb9674597d69e8a8efc94663b75b8
amarchino <marchino.alessandro@gmail.com>
2026-02-25 22:20:52 +0100

    Lesson 438

--- spring7restmvc/src/main/java/guru/springframework/spring7restmvc/mappers/BeerMapper.java
+++ spring7restmvc/src/main/java/guru/springframework/spring7restmvc/mappers/BeerMapper.java
@@ -1,13 +1,24 @@
 package guru.springframework.spring7restmvc.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import guru.springframework.spring7restmvc.entities.Beer;
+import guru.springframework.spring7restmvc.entities.BeerAudit;
 import guru.springframework.spring7restmvc.model.BeerDTO;
 
 @Mapper
 public interface BeerMapper {
 
+	@Mapping(target = "categories", ignore = true)
+	@Mapping(target = "beerOrderLines", ignore = true)
 	Beer beerDtoToBeer(BeerDTO dto);
+
 	BeerDTO beerToBeerDto(Beer beer);
+
+	@Mapping(target = "createdDateAudit", ignore = true)
+	@Mapping(target = "auditId", ignore = true)
+	@Mapping(target = "auditEventType", ignore = true)
+	@Mapping(target = "principalName", ignore = true)
+	BeerAudit beerToBeerAudit(Beer beer);
 }
